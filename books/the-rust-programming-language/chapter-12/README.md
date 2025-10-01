80a7c90fc1d38fefddb4b1b6bce873715d430519
Fabio Ueno <fabio.t.ueno@gmail.com>
2025-10-01 09:55:53 +0200

    chore(tlpr): continue twelfth chapter

--- books/the-rust-programming-language/chapter-12/README.md
+++ books/the-rust-programming-language/chapter-12/README.md
@@ -6,7 +6,7 @@ Programming Language".
 ## Progress
 
 - [x] Accepting Command Line Arguments
-- [ ] Reading a File
+- [x] Reading a File
 - [ ] Refactoring to Improve Modularity and Error Handling
 - [ ] Developing the Library's Functionality with Test-Driven Development
 - [ ] Working with Environment Variables
--- books/the-rust-programming-language/chapter-12/minigrep/src/main.rs
+++ books/the-rust-programming-language/chapter-12/minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // How can we prevent the user from passing a very large number of
@@ -11,4 +12,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{}", contents);
 }
